ee2859485b200c72c0b1f6f05e9ec8adbfafc3ab
smwbtech <smwb.tech@gmail.com>
2020-02-16 15:30:44 +0300

    Update VueTextTransitionLetter.ts
    * Added data.baseClass
    * Added computed visibleClass, hiddenClass, regularClass
    * Added setting baseClass in created hook
    * Added setting up baseClass from props.customClass in mounted hook

--- src/components/VueTextTransitionLetter.ts
+++ src/components/VueTextTransitionLetter.ts
@@ -1,15 +1,45 @@
 import { VNode, CreateElement } from 'vue'
 import { Component, Prop, Vue } from 'vue-property-decorator'
+import { CustomSelectorData } from 'vue-text-transition'
 
 @Component
 export default class VueTextTransitionLetter extends Vue {
   @Prop({ default: false }) readonly show!: boolean
   @Prop({ default: 100 }) readonly interval!: number
-  @Prop() readonly hidden!: string
-  @Prop() readonly visible!: string
-  @Prop() readonly regularClass!: string
-  render(createElement: CreateElement): VNode {
-    return createElement(
+  @Prop({ required: true }) readonly className!: string
+  @Prop({ default: null }) readonly customClass?: CustomSelectorData[] | null
+
+  baseClass = ''
+
+  created() {
+    this.baseClass = this.$props.className
+  }
+
+  mounted() {
+    if (this.$props.customClass) {
+      for (const item of this.$props.customClass) {
+        if (this.$el.matches(item.selector)) {
+          this.baseClass = item.class
+          break
+        }
+      }
+    }
+  }
+
+  get visibleClass(): string {
+    return `v--${this.baseClass}_visible`
+  }
+
+  get hiddenClass(): string {
+    return `v--${this.baseClass}_hidden`
+  }
+
+  get regularClass(): string {
+    return `v--${this.baseClass}`
+  }
+
+  render(h: CreateElement): VNode {
+    const letter = h(
       'span',
       {
         style: {
@@ -18,11 +48,12 @@ export default class VueTextTransitionLetter extends Vue {
           display: this.$slots.default ? 'inline-block' : 'inline'
         },
         class: [
-          this.$props.regularClass,
-          this.$props.show ? this.$props.visible : this.$props.hidden
+          this.regularClass,
+          this.$props.show ? this.visibleClass : this.hiddenClass
         ]
       },
       [this.$slots.default || ' ']
     )
+    return letter
   }
 }
